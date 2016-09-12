a2925076f0a6fb9c6c717258a74e63ac9ffd9dbb
cjsheehan <Chris.Sheehan@ymail.com>
2016-09-12 15:52:00 +0100

    initial setup

---
+++ RaceAnalyser/src/main/java/com/cjsheehan/jrace/App.java
@@ -0,0 +1,13 @@
+package com.cjsheehan.jrace;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
