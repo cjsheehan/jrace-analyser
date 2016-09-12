7c8f993e94097c29bfa6c2667705d42b33290ff7
cjsheehan <Chris.Sheehan@ymail.com>
2016-09-12 16:51:16 +0100

    basic jsoup extraction of horse names from RP

--- RaceAnalyser/src/main/java/com/cjsheehan/jrace/App.java
+++ RaceAnalyser/src/main/java/com/cjsheehan/jrace/App.java
@@ -1,13 +1,51 @@
 package com.cjsheehan.jrace;
 
-/**
- * Hello world!
- *
- */
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import com.cjsheehan.jrace.racing.Horse;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String url = "http://www.racingpost.com/horses2/cards/card.sd?race_id=657700&r_date=2016-09-12#raceTabs=sc_";
+        
+        Document doc = null;
+        try
+        {
+            System.out.println("Fetching %s..." +  url);
+            doc = Jsoup.connect(url)
+                    .userAgent("Mozilla/5.0")
+                    .timeout(3000)
+                    .get();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        
+        Elements entrants = doc.select("table#sc_horseCard > tbody a > b");
+        List<Horse> horses = new ArrayList<>();
+        
+        for(Element entrant: entrants) {
+            Horse h = new Horse();
+            h.setName(entrant.ownText());
+            horses.add(h);
+        }
+        
+        for (Horse horse : horses)
+        {
+            System.out.println(horse.toString());
+        }
+        
     }
 }
+
+
---
+++ RaceAnalyser/src/main/java/com/cjsheehan/jrace/racing/Horse.java
@@ -0,0 +1,20 @@
+package com.cjsheehan.jrace.racing;
+
+public class Horse
+{
+    private String name;
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    
+    public String toString() { 
+        return "Name: " + this.name;
+    } 
+}
