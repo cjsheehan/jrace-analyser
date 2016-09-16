3eaa6174e2565e9d0cdde734df73ea8022e6c153
cjsheehan <Chris.Sheehan@ymail.com>
2016-09-16 19:18:44 +0100

    use local html for testing scraper

--- RaceAnalyser/src/main/java/com/cjsheehan/jrace/App.java
+++ RaceAnalyser/src/main/java/com/cjsheehan/jrace/App.java
@@ -1,13 +1,33 @@
 package com.cjsheehan.jrace;
 
-import com.cjsheehan.jrace.racing.Race;
+import java.io.IOException;
+
+import org.jsoup.nodes.Document;
+
+import com.cjsheehan.jrace.scrape.Html;
+import com.cjsheehan.jrace.scrape.ScrapeException;
 import com.cjsheehan.jrace.scrape.rpost.Card;
 
 public class App {
     public static void main(String[] args) {
-	String url = "http://www.racingpost.com/horses2/cards/card.sd?race_id=657553&r_date=2016-09-13#raceTabs=sc_";
-	Card card = new Card();
-	Race race = card.scrape(url);
-	System.out.println("Race: \n" + race.toString());
+//	String url = "http://www.racingpost.com/horses2/cards/card.sd?race_id=657619&r_date=2016-09-16";
+	String localUrl = "./html/card.html";
+	Document doc = null;
+	try {
+	    doc = Html.toDocument(localUrl);
+	    Card card = null;
+	    try {
+		card = new Card(doc);
+	    } catch (ScrapeException e) {
+		System.out.println("ERROR: \n" + e.getMessage());
+		e.printStackTrace();
+	    }
+	    
+	    System.out.println("RaceCard: \n" + card.toString());
+	    
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+
     }
 }
