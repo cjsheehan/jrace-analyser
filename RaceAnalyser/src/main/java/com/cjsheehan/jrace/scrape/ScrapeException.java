b1811653bd6ee9b52022c7179689adba762d775b
cjsheehan <Chris.Sheehan@ymail.com>
2016-09-16 16:56:00 +0100

    added serial id

--- RaceAnalyser/src/main/java/com/cjsheehan/jrace/scrape/ScrapeException.java
+++ RaceAnalyser/src/main/java/com/cjsheehan/jrace/scrape/ScrapeException.java
@@ -1,6 +1,7 @@
 package com.cjsheehan.jrace.scrape;
 
 public class ScrapeException extends Exception {
+    private static final long serialVersionUID = 1L;
     public ScrapeException(String message) {
 	super(message);
     }
