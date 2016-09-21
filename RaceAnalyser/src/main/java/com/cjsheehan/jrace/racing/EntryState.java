07dbe83d08808855c5a89960eb516f97ea20129f
cjsheehan <Chris.Sheehan@ymail.com>
2016-09-21 16:17:58 +0100

    Required for ResultEntrant handling

---
+++ RaceAnalyser/src/main/java/com/cjsheehan/jrace/racing/EntryState.java
@@ -0,0 +1,16 @@
+package com.cjsheehan.jrace.racing;
+
+public enum EntryState {
+    FINISHED,
+    UNSEATED_RIDER,
+    PULLED_UP,
+    FELL,
+    REFUSED,
+    RAN_OUT,
+    BROUGHT_DOWN,
+    SLIPPED,
+    DISQUALIFIED,
+    VOID,
+    NON_RUNNER,
+    NOT_AVAILABLE
+}
