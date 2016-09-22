c0362fe62dd0da8b680f1e15fa487f40a9f20a6d
cjsheehan <Chris.Sheehan@ymail.com>
2016-09-22 15:36:28 +0100

    use local or remote files for scraping

--- RaceAnalyser/src/main/java/com/cjsheehan/jrace/App.java
+++ RaceAnalyser/src/main/java/com/cjsheehan/jrace/App.java
@@ -3,27 +3,37 @@ package com.cjsheehan.jrace;
 import java.io.IOException;
 
 import org.jsoup.nodes.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.cjsheehan.jrace.scrape.Html;
 import com.cjsheehan.jrace.scrape.ScrapeException;
 import com.cjsheehan.jrace.scrape.rpost.Card;
+import com.cjsheehan.jrace.scrape.rpost.Result;
 
 public class App {
+    @SuppressWarnings("unused")
     public static void main(String[] args) {
-//	String url = "http://www.racingpost.com/horses2/cards/card.sd?race_id=657619&r_date=2016-09-16";
-	String localUrl = "./html/card.html";
+	final Logger logger = LoggerFactory.getLogger(App.class);
+	String remoteCardUrl = "http://www.racingpost.com/horses2/cards/card.sd?race_id=659479&r_date=2016-09-19";
+	String localCardUrl = "./html/card_race_id_657619.html";
+	
+	String remoteResultUrl = "http://www.racingpost.com/horses/result_home.sd?race_id=642825&r_date=2016-04-09";
+	String localResultUrl = "./html/result_race_id_657619.html";
+	
+	String url = remoteResultUrl;
 	Document doc = null;
 	try {
-	    doc = Html.toDocument(localUrl);
-	    Card card = null;
+	    doc = Html.toDocument(url);
+	    Result result = null;
 	    try {
-		card = new Card(doc);
+		result = new Result(doc);
 	    } catch (ScrapeException e) {
 		System.out.println("ERROR: \n" + e.getMessage());
 		e.printStackTrace();
 	    }
 	    
-	    System.out.println("RaceCard: \n" + card.toString());
+	    System.out.println("Result: \n" + result.toString());
 	    
 	} catch (IOException e) {
 	    e.printStackTrace();
