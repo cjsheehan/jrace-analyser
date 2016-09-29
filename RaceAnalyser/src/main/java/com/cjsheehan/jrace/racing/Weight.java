e6e695634f973a5a687e8af5e3b790e5f5cb9ee9
cjsheehan <Chris.Sheehan@ymail.com>
2016-09-29 20:59:38 +0100

    Weight conversions

---
+++ RaceAnalyser/src/main/java/com/cjsheehan/jrace/racing/Weight.java
@@ -0,0 +1,40 @@
+package com.cjsheehan.jrace.racing;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.jsoup.helper.StringUtil;
+
+public class Weight {
+
+    static final int LBS_PER_STONE = 14;
+    static final Pattern pToLbs = Pattern.compile("^(\\d+)-(\\d+)$");
+    
+    public static int toLbs(String weight) {
+	if(StringUtil.isBlank(weight)) {
+	    throw new IllegalArgumentException("weight is blank/null");
+	}
+	
+	int totalLbs = 0;
+	Matcher m = pToLbs.matcher(weight);
+	if(m.find()) {
+	    int st = Integer.parseInt(m.group(1));
+	    int lbs = Integer.parseInt(m.group(2)); 
+	    totalLbs = (LBS_PER_STONE * st) + lbs;
+	} else {
+	    throw new IllegalArgumentException("weight must be in the form x-y");
+	}
+	
+	return totalLbs;
+    }
+    
+    public static String toStAndLbs(int lbs) {
+	if(lbs < 0) {
+	    throw new IllegalArgumentException("lbs must be > 0");
+	}
+	
+	int st = lbs / LBS_PER_STONE;
+	lbs = lbs % LBS_PER_STONE;
+	return st + "-" + lbs;
+    }
+}
