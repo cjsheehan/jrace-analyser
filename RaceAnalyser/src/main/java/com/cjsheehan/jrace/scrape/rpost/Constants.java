654ee0530f3cbc093dd4af242071efc90e71795a
cjsheehan <Chris.Sheehan@ymail.com>
2016-10-08 15:30:01 +0100

    Missing required constant for for forming urls

--- RaceAnalyser/src/main/java/com/cjsheehan/jrace/scrape/rpost/Constants.java
+++ RaceAnalyser/src/main/java/com/cjsheehan/jrace/scrape/rpost/Constants.java
@@ -5,4 +5,7 @@ public class Constants {
     public static final int MAX_WAIT = 3000; // http get max wait in ms
     public static final String USER_AGENT = "Mozilla/5.0";
     public static final String ROOT_URL = "http://www.racingpost.com/";
+    public static final String RESULT_URL = ROOT_URL + "horses/result_home.sd";
+    public static final String CARD_URL = ROOT_URL + "horses2/cards/card.sd";
+    
 }
